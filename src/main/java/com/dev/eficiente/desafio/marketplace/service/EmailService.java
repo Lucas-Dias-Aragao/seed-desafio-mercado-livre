cea7e4f36c0ba62aa4b68858331bb68df610b5cf
LucasDias <lucasd.aragao@hotmail.com>
2026-06-14 19:44:28 -0300

    feat: implementa preparação para enviar email ao vendedor

--- src/main/java/com/dev/eficiente/desafio/marketplace/service/EmailService.java
+++ src/main/java/com/dev/eficiente/desafio/marketplace/service/EmailService.java
@@ -7,5 +7,14 @@ public class EmailService {
 
     public void enviarEmail(final String emailDestinatario, final String assuntoEmail, final String corpoEmail) {
 
+        String email = """
+                Destinatário: %s
+                
+                Assunto: %s
+                
+                Mensagem: %s
+                """.formatted(emailDestinatario, assuntoEmail, corpoEmail);
+
+        System.out.println(email);
     }
 }
